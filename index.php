42d20d4a2e560871e2a729b21fed7eda28c147ae
Angolatiana2006 <tianarandianina3@gmail.com>
2026-02-12 11:56:41 +0300

    assets

--- index.php
+++ index.php
@@ -5,14 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-    jjj
-<<<<<<< HEAD
-    bbb
- kkkk
-    kkk
+  
 
-
-    jjj
->>>>>>> a5532119c3bf71453a12f8739dee066d1540be5d
 </body>
 </html>
